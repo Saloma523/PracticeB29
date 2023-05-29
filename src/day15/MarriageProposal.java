35980538ec79028ac60f50e7d38c49e87979e45b
Salma <saloma523@yahoo.com>
2023-05-29 01:45:20 -0400

    Marriage proposal task is done

--- src/day15/MarriageProposal.java
+++ src/day15/MarriageProposal.java
@@ -1,7 +1,39 @@
 package day15;
 
+import java.util.Scanner;
+
 public class MarriageProposal {
     public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println( "Will you marry me?");
+
+        String answer = input.next();
+
+        while (!(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no"))){
+
+            System.out.println("Invalid answer, please re-enter" );
+
+            answer = input.next();
+
+
+
+
+
+        }
+
+        if(answer.equalsIgnoreCase("yes")){
+            System.out.println("Congrats");
+        }else{
+            System.out.println("Goodbye");
+        }
+
+
+
+        input.close();
+
 
     }
     /*
