8772fe432621b0d8b70fca407730c853a7de7277
rayyan609 <75813959+rayyan609@users.noreply.github.com>
2023-05-29 06:25:17 -0400

    Updated frequencyOfChart

--- src/day15/FrequencyOfChart.java
+++ src/day15/FrequencyOfChart.java
@@ -1,12 +1,33 @@
+package day15;
+
 import java.util.Scanner;
 
 public class FrequencyOfChart {
     public static void main(String[] args) {
+          String str = "aabcccd";
+        char ch= 'c' ;
 
-        Scanner input = new Scanner(System.in);
+      /*  Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string");
+        str = input.nextLine();
+        System.out.println("Enter a character");
+        ch = input.nextLine();
+      */
+
+        int count= 0;
+
+        for (int i =0; i< str.length(); i++) {
+
+            if (str.charAt(i) == ch){
+                count++;
+            }
+        }
+            System.out.println(count);
+    }
+       /* Scanner input = new Scanner(System.in);
         System.out.println("Enter a string");
         String
-    }
+      */
 }
 /*Create a class named FrequencyOfChar and Write a program
  that asks user to enter a string and a char,
